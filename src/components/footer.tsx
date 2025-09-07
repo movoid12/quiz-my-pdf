d564db33dcf87b50f8d441ed05116cd610225abb
Mouaz Aldakkak <87415713+movoid12@users.noreply.github.com>
2025-09-08 00:05:52 +0200

    cleanup

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="footer footer-center mt-8 p-4 bg-base-200 text-base-content border-t-2 border-base-content/10">
       <div>
-        <p>Made with ❤️&☕ by Mouaz</p>{' '}
+        <p>Made with ❤️&☕ by Mouaz</p>
       </div>
     </footer>
   );
